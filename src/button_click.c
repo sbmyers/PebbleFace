8a8fe242e1523d3aee59e82bb066ab8d1752410a
sbmyers <s.myers@computer.org>
2014-06-30 04:54:19 -0700

    Corrected GetHours
    Changed to switch instead of math.

--- src/button_click.c
+++ src/button_click.c
@@ -211,7 +211,33 @@ static time_t Seconds(struct tm *pTime){
   return 60 * (pTime->tm_hour * 60 + pTime->tm_min);
 }
 static int GetHour(struct tm *pTime){
-  return ((pTime->tm_hour - 1) % 12) + 1;
+    switch (pTime->tm_hour){
+        case 0:     return 12;
+        case 1:     return 1;
+        case 2:     return 2;
+        case 3:     return 3;
+        case 4:     return 4;
+        case 5:     return 5;
+        case 6:     return 6;
+        case 7:     return 7;
+        case 8:     return 8;
+        case 9:     return 9;
+        case 10:    return 10;
+        case 11:    return 11;
+        case 12:    return 12;
+        case 13:    return 1;
+        case 14:    return 2;
+        case 15:    return 3;
+        case 16:    return 4;
+        case 17:    return 5;
+        case 18:    return 6;
+        case 19:    return 7;
+        case 20:    return 8;
+        case 21:    return 9;
+        case 22:    return 10;
+        case 23:    return 11;
+    }
+    return ((pTime->tm_hour - 1) % 12) + 1;
 }
 static void ShowIt(){
   time_t target = 0;
